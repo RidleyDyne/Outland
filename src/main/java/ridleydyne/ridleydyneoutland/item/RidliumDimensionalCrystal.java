2547be75038f01af907c74d49266e00115ab8f2d
Mark Strendin <mark.strendin@gmail.com>
2021-01-12 00:02:02 -0600

    Add a new dimension

--- src/main/java/ridleydyne/ridleydyneoutland/item/RidliumDimensionalCrystal.java
+++ src/main/java/ridleydyne/ridleydyneoutland/item/RidliumDimensionalCrystal.java
@@ -1,11 +1,21 @@
 package ridleydyne.ridleydyneoutland.item;
 
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import ridleydyne.ridleydyneoutland.init.ModItems;
 
 public class RidliumDimensionalCrystal extends Item {
     public RidliumDimensionalCrystal() {
         super(ModItems.defaultItemProperties(64));
+    }
+
+    @Override
+    public void onUse(World worldIn, LivingEntity livingEntityIn, ItemStack stack, int count) {
+        // This is currently debug code for testing purposes
+        // livingEntityIn.changeDimension(worldIn.getServer(), );
+
     }
 
 }
